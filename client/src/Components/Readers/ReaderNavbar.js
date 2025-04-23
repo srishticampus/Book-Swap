ccda1845666122cc5a4aef8e9578c14e414ab672
Anandhu <anandhujs.srishti@gmail.com>
2025-04-23 09:58:29 +0530

    new

--- client/src/Components/Readers/ReaderNavbar.js
+++ client/src/Components/Readers/ReaderNavbar.js
@@ -4,17 +4,49 @@ import { TiHome } from "react-icons/ti";
 import { BsPersonCircle } from "react-icons/bs";
 import "./reader.css";
 import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 function ReaderNavbar() {
   return (
     <div>
       <nav className="navbar" id="navbackground">
-        <div className="container">
-          <img src={logo} alt="img" width="120" height="90"></img>
-          <span class="navbar-text">
-            <Link to='/' ><TiHome className="userhomeicon" /></Link>
-            <Link to='/reader_loginpage' ><BsPersonCircle className="userprofileicon" /></Link>
-          </span>
+        <div className="container d-flex justify-content-between align-items-center">
+          <img src={logo} alt="logo" width="120" height="90" />
+
+          <div className="d-flex align-items-center gap-3">
+            <Link to="/">
+              <TiHome className="userhomeicon" />
+            </Link>
+
+            {/* Bootstrap Dropdown */}
+            <div className="dropdown">
+  <button
+    className="btn btn-link dropdown-toggle p-0 border-0"
+    id="userDropdown"
+    data-bs-toggle="dropdown"
+    aria-expanded="false"
+  >
+    <BsPersonCircle className="userprofileicon" size={28} />
+  </button>
+  <ul
+    className="dropdown-menu dropdown-menu-end"
+    aria-labelledby="userDropdown"
+  >
+    <li>
+      <Link className="dropdown-item" to="/reader_loginpage">
+        User Login
+      </Link>
+    </li>
+    <li>
+      <Link className="dropdown-item" to="/club_loginpage">
+        Library Login
+      </Link>
+    </li>
+  </ul>
+</div>
+
+          </div>
         </div>
       </nav>
     </div>
@@ -22,3 +54,31 @@ function ReaderNavbar() {
 }
 
 export default ReaderNavbar;
+
+
+
+// import React from "react";
+// import logo from "../../Assets/logo.png";
+// import { TiHome } from "react-icons/ti";
+// import { BsPersonCircle } from "react-icons/bs";
+// import "./reader.css";
+// import { Link } from "react-router-dom";
+
+// function ReaderNavbar() {
+//   return (
+//     <div>
+//       <nav className="navbar" id="navbackground">
+//         <div className="container">
+//           <img src={logo} alt="img" width="120" height="90"></img>
+//           <span class="navbar-text">
+//             <Link to='/' ><TiHome className="userhomeicon" /></Link>
+//             <Link to='/reader_loginpage' ><BsPersonCircle className="userprofileicon" /></Link>
+//           </span>
+//         </div>
+//       </nav>
+//     </div>
+//   );
+// }
+
+// export default ReaderNavbar;
+
