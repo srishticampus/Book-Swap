3a79bd20a659dc03b8458332e316d860355ac489
ANANDHUJS-Srishti <anandhujs.srishti@gmail.com>
2025-07-02 13:06:26 +0530

    new

--- client/src/Components/Pages/LoginNavbar.js
+++ client/src/Components/Pages/LoginNavbar.js
@@ -1,14 +1,22 @@
 import React from "react";
 import logo from "../../Assets/logo.png";
-
+import { TiHome } from "react-icons/ti";
+import { Link } from "react-router-dom";
 function LoginNavbar() {
   return (
-    <div>
-      <nav className="navbar text-white">
-        <div className="container">
-          <img src={logo} alt="img" width="120" height="90"></img>
-        </div>
-      </nav>
+    <div className="d-flex justify-content-between">
+      <div>
+        <nav className="navbar text-white">
+          <div className="container">
+            <img src={logo} alt="img" width="120" height="90"></img>
+          </div>
+        </nav>
+      </div>
+      <div className="mt-3">
+        <Link to="/">
+          <TiHome className="userhomeicon" />
+        </Link>
+      </div>
     </div>
   );
 }
