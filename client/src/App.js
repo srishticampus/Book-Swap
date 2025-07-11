a9b4013deea716a4f3d18e9ce5fedfc5a33e6edd
ANANDHUJS-Srishti <anandhujs.srishti@gmail.com>
2025-07-11 09:37:54 +0530

    new

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+// import "./index.css"
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import 'remixicon/fonts/remixicon.css'
